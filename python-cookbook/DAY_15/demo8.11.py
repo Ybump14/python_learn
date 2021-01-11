441bf15b626e7bd66cc0905a5f3ab3a69cb57632
cy <310486578@qq.com>
2021-01-11 16:16:19 +0800

    python-cookbook

--- python-cookbook/DAY_15/demo8.11.py
+++ python-cookbook/DAY_15/demo8.11.py
@@ -75,30 +75,45 @@ class Structure3:
             raise TypeError('Duplicate values for {}'.format(','.join(kwargs)))
 
 
+class Structure:
+    # Class variable that specifies expected fields
+    _fields = []
+
+    def __init__(self, *args):
+        if len(args) != len(self._fields):
+            raise TypeError('Expected {} arguments'.format(len(self._fields)))
+
+        # Set the arguments (alternate)
+        self.__dict__.update(zip(self._fields, args))
+
+
 # Example use
 if __name__ == '__main__':
     # Example 1
-    # s = Stock('ACME', 50, 91.1)
-    # print(s.name, s.shares, s.price, sep=' ')  # ACME 50 91.1
+    s = Stock('ACME', 50, 91.1)
+    print(s.name, s.shares, s.price, sep=' ')  # ACME 50 91.1
     #
-    # p = Point(2, 3)  # 2 3
-    # print(p.x, p.y)
+    p = Point(2, 3)  # 2 3
+    print(p.x, p.y)
     #
-    # c = Circle('radius')  # radius
-    # print(c.radius)
+    c = Circle('radius')  # radius
+    print(c.radius)
+
 
     # Example 2
     class Stock(Structure2):
         _fields = ['name', 'shares', 'price']
 
 
-    # s1 = Stock('ACME', 50, 91.1)
-    # print(s1.name, s1.shares, s1.price)
-    # s2 = Stock('ACME', 50, price=91.1)
-    # print(s2.name, s2.shares, s2.price)
-    # s3 = Stock('ACME', shares=50, price=91.1)
-    # print(s3.name, s3.shares, s3.price)
+    s1 = Stock('ACME', 50, 91.1)
+    print(s1.name, s1.shares, s1.price)
+    s2 = Stock('ACME', 50, price=91.1)
+    print(s2.name, s2.shares, s2.price)
+    s3 = Stock('ACME', shares=50, price=91.1)
+    print(s3.name, s3.shares, s3.price)
 
+
+    #
     # s3 = Stock('ACME', shares=50, price=91.1, aa=1)
     # print(s3.name, s3.shares, s3.price)
     # raise TypeError('Invalid argument(s): {}'.format(','.join(kwargs)))
@@ -109,6 +124,20 @@ if __name__ == '__main__':
         _fields = ['name', 'shares', 'price']
 
 
-    # s1 = Stock('ACME', 50, 91.1)
+    s1 = Stock('ACME', 50, 91.1)
     s2 = Stock('ACME', 50, 91.1, date='8/2/2012', MyName='YANGJIAJUN')
     print(s2.name, s2.shares, s2.price, s2.MyName, sep=' ')  # ACME 50 91.1 YANGJIAJUN
+
+
+    # Example 4
+    class Stock(Structure):
+        # Stock定义了 __slots__ 或者通过property(或描述器)来包装某个属性
+        # 那么直接访问实例字典(Structure)就不起作用了
+        __slots__ = ['name']
+        _fields = ['name', 'shares', 'price']
+
+
+    s1 = Stock('ACME', 50, 91.1)
+    # print(s1.name, s1.shares, s1.price)  # AttributeError: name
+    # s2 = Stock('ACME', 50, 91.1, date='8/2/2012', MyName='YANGJIAJUN')
+    # print(s2.name, s2.shares, s2.price, s2.MyName, sep=' ')  # ACME 50 91.1 YANGJIAJUN
