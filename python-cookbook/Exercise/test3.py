31f2329c785e3b49aeeedd1dad0ebcdfd6663d35
cy <310486578@qq.com>
2021-01-21 10:44:36 +0800

    python-cookbook

--- python-cookbook/Exercise/test3.py
+++ python-cookbook/Exercise/test3.py
@@ -10,7 +10,10 @@ class DateUtil:
         self.month = month
         self.day = day
 
-    def DayStamp(self):
+    def __repr__(self):
+        return str(datetime(year=self.year, month=self.month, day=self.day))
+
+    def dayStamp(self):
         stamp = datetime(year=self.year, month=self.month, day=self.day)
         sstamp = int(time.mktime(
             time.strptime(str(stamp.replace(hour=0, minute=0, second=0)), "%Y-%m-%d %H:%M:%S"))) * 1000
@@ -18,7 +21,7 @@ class DateUtil:
                                                "%Y-%m-%d %H:%M:%S"))) * 1000
         return sstamp, estamp
 
-    def Month_Stamp(slef):
+    def monthStamp(slef):
         firstDayWeekDay, monthRange = calendar.monthrange(slef.year, slef.month)
         firstDay = datetime(year=slef.year, month=slef.month, day=1)
         lastDay = datetime(year=firstDay.year, month=firstDay.month, day=monthRange)
@@ -28,39 +31,56 @@ class DateUtil:
         return firstDay, lastDay
 
     @classmethod
-    def thisDayStamp(cls):
-        sstamp = int(time.mktime(
+    def dayFirstStamp(cls):
+        stamp = int(time.mktime(
             time.strptime(str(datetime.fromtimestamp(round(time.time())).replace(hour=0, minute=0, second=0)),
                           "%Y-%m-%d %H:%M:%S"))) * 1000
-        estamp = int(time.mktime(
+        return stamp
+
+    @classmethod
+    def dayLastStamp(cls):
+        stamp = int(time.mktime(
             time.strptime(str(datetime.fromtimestamp(round(time.time())).replace(hour=23, minute=59, second=59)),
                           "%Y-%m-%d %H:%M:%S"))) * 1000
-        return sstamp, estamp
+        return stamp
 
     @classmethod
-    def thisMonthStamp(cls):
+    def monthFirstStamp(cls):
+        stamp = datetime(year=datetime.now().year, month=datetime.now().month, day=1)
+        stamp = int(time.mktime(time.strptime(str(stamp), "%Y-%m-%d %H:%M:%S"))) * 1000
+        return stamp
+
+    @classmethod
+    def monthLastStamp(cls):
         firstDayWeekDay, monthRange = calendar.monthrange(datetime.now().year, datetime.now().month)
-        thisMonth_firstDay = datetime(year=datetime.now().year, month=datetime.now().month, day=1)
-        thisMonth_lastDay = datetime(year=datetime.now().year, month=datetime.now().month, day=monthRange)
-        thisMonth_firstDay = int(time.mktime(time.strptime(str(thisMonth_firstDay), "%Y-%m-%d %H:%M:%S"))) * 1000
-        thisMonth_lastDay = int(time.mktime(
-            time.strptime(str(thisMonth_lastDay.replace(hour=23, minute=59, second=59)), "%Y-%m-%d %H:%M:%S"))) * 1000
-        return thisMonth_firstDay, thisMonth_lastDay
+        stamp = datetime(year=datetime.now().year, month=datetime.now().month, day=monthRange)
+        stamp = int(time.mktime(
+            time.strptime(str(stamp.replace(hour=23, minute=59, second=59)), "%Y-%m-%d %H:%M:%S"))) * 1000
+        return stamp
 
     @classmethod
-    def lastMonthStamp(cls):
-        lastMonth_lastDay = datetime(year=datetime.now().year, month=datetime.now().month, day=1) - timedelta(days=1)
-        lastMonth_firstDay = datetime(year=lastMonth_lastDay.year, month=lastMonth_lastDay.month, day=1)
-        lastMonth_firstDay = int(time.mktime(time.strptime(str(lastMonth_firstDay), "%Y-%m-%d %H:%M:%S"))) * 1000
-        lastMonth_lastDay = int(time.mktime(
-            time.strptime(str(lastMonth_lastDay.replace(hour=23, minute=59, second=59)), "%Y-%m-%d %H:%M:%S"))) * 1000
-        return lastMonth_firstDay, lastMonth_lastDay
+    def lastMonthFirstStamp(cls):
+        stamp = datetime(year=datetime.now().year, month=datetime.now().month, day=1) - timedelta(days=1)
+        stamp = datetime(year=stamp.year, month=stamp.month, day=1)
+        stamp = int(time.mktime(time.strptime(str(stamp), "%Y-%m-%d %H:%M:%S"))) * 1000
+        return stamp
+
+    @classmethod
+    def lastMonthLastStamp(cls):
+        stamp = datetime(year=datetime.now().year, month=datetime.now().month, day=1) - timedelta(days=1)
+        stamp = int(time.mktime(
+            time.strptime(str(stamp.replace(hour=23, minute=59, second=59)), "%Y-%m-%d %H:%M:%S"))) * 1000
+        return stamp
 
 
-print(DateUtil.thisMonthStamp()[0])
-print(DateUtil.thisMonthStamp()[1])
-print(DateUtil.lastMonthStamp()[0])
-print(DateUtil.lastMonthStamp()[1])
+print(DateUtil.dayFirstStamp())
+print(DateUtil.dayLastStamp())
 
-print(DateUtil(2020, 2, 2).Month_Stamp()[0])
-print(DateUtil(2020, 2, 2).Month_Stamp()[1])
+print(DateUtil.monthFirstStamp())
+print(DateUtil.monthLastStamp())
+
+print(DateUtil.lastMonthFirstStamp())
+print(DateUtil.lastMonthLastStamp())
+
+print(DateUtil(2020, 2, 2).monthStamp())
+print(DateUtil(2021, 2, 2).monthStamp())
