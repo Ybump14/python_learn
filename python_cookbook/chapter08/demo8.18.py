6f33f5cfbc869d1a29777987c72b5d358c8562b3
cy <310486578@qq.com>
2021-03-17 09:44:30 +0800

    python-cookbook

--- python_cookbook/chapter08/demo8.18.py
+++ python_cookbook/chapter08/demo8.18.py
@@ -64,3 +64,32 @@ class SetOnceDefaultDict(SetOnceMappingMixin, defaultdict):
 
 class StringKeys(StringKeysMappingMixin, defaultdict):
     pass
+
+
+def LoggedMapping(cls):
+    """第二种方式：使用类装饰器"""
+    cls_getitem = cls.__getitem__
+    cls_setitem = cls.__setitem__
+    cls_delitem = cls.__delitem__
+
+    def __getitem__(self, key):
+        print('Getting ' + str(key))
+        return cls_getitem(self, key)
+
+    def __setitem__(self, key, value):
+        print('Setting {} = {!r}'.format(key, value))
+        return cls_setitem(self, key, value)
+
+    def __delitem__(self, key):
+        print('Deleting ' + str(key))
+        return cls_delitem(self, key)
+
+    cls.__getitem__ = __getitem__
+    cls.__setitem__ = __setitem__
+    cls.__delitem__ = __delitem__
+    return cls
+
+
+@LoggedMapping
+class LoggedDict(dict):
+    pass
