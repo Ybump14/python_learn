63d883be8afd3f998b9bcac984c5a4b647c5723b
cy <310486578@qq.com>
2021-03-18 14:31:25 +0800

    python-cookbook

---
+++ python_cookbook/chapter08/demo8.21.py
@@ -0,0 +1,95 @@
+'''8.21 实现访问者模式
+问题:
+你要处理由大量不同类型的对象组成的复杂数据结构，每一个对象都需要进行不同的处理。
+比如，遍历一个树形结构，然后根据每个节点的相应状态执行不同的操作。
+
+解决方案:
+这里遇到的问题在编程领域中是很普遍的，有时候会构建一个由大量不同对象组成的数据结构。
+假设你要写一个表示数学表达式的程序，那么你可能需要定义如下的类：'''
+
+
+class Node:
+    pass
+
+
+class UnaryOperator(Node):
+    def __init__(self, operand):
+        self.operand = operand
+
+
+class BinaryOperator(Node):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+
+class Add(BinaryOperator):
+    pass
+
+
+class Sub(BinaryOperator):
+    pass
+
+
+class Mul(BinaryOperator):
+    pass
+
+
+class Div(BinaryOperator):
+    pass
+
+
+class Negate(UnaryOperator):
+    pass
+
+
+class Number(Node):
+    def __init__(self, value):
+        self.value = value
+
+
+class NodeVisitor:
+    def visit(self, node):
+        methname = 'visit_' + type(node).__name__
+        meth = getattr(self, methname, None)
+        if meth is None:
+            meth = self.generic_visit
+        return meth(node)
+
+    def generic_visit(self, node):
+        raise RuntimeError('No {} method'.format('visit_' + type(node).__name__))
+
+
+class Evaluator(NodeVisitor):
+    def visit_Number(self, node):
+        return node.value
+
+    def visit_Add(self, node):
+        return self.visit(node.left) + self.visit(node.right)
+
+    def visit_Sub(self, node):
+        return self.visit(node.left) - self.visit(node.right)
+
+    def visit_Mul(self, node):
+        return self.visit(node.left) * self.visit(node.right)
+
+    def visit_Div(self, node):
+        return self.visit(node.left) / self.visit(node.right)
+
+    def visit_Negate(self, node):
+        return -node.operand
+
+
+t1 = Add(Number(3), Number(4))
+t2 = Sub(Number(3), Number(4))
+t3 = Mul(Number(3), Number(4))
+t4 = Div(3, 4)
+print(t4.left)
+print(t4.right)
+t5 = Negate(-3)
+e = Evaluator()
+print(e.visit(t1))
+print(e.visit(t2))
+print(e.visit(t3))
+# print(e.visit(t4))
+print(e.visit(t5))
