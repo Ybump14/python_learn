eb1ec48b8ed2ea769dc8432679c652eade8c021f
Ybump14 <310486578@qq.com>
2021-11-02 18:58:44 +0800

    commit

--- pyserial/message.py
+++ pyserial/message.py
@@ -1,38 +1,32 @@
-CON_ON = '+EVENT=CON_ON'
-CON_ON_MESSAGE = 'STA 成功连接到路由器 '
-
-CON_OFF = '+EVENT=CON_OFF'
-CON_OFF_MESSAGE = 'STA 断开路由器或者未连接到路由器 '
-
-DHCP_OK = '+EVENT=DHCP_OK'
-DHCP_OK_MESSAGE = 'STA DHCP获取到IP'
-
-SOCKA_ON = '+EVENT=SOCKA_ON'
-SOCKA_ON_MESSAGE = 'SOCKA连接建立(仅TCP Client/Server,MQTT,HTTP)'
-
-SOCKA_OFF = '+EVENT=SOCKA_OFF'
-SOCKA_OFF_MESSAGE = 'SOCKA连接断开(仅TCP Client/Server,MQTT,HTTP)'
-
-SOCKB_ON = '+EVENT=SOCKB_ON'
-SOCKB_ON_MESSAGE = 'SOCKB连接建立(仅TCP Client)'
-
-SOCKB_OFF = '+EVENT=SOCKB_OFF'
-SOCKB_OFF_MESSAGE = 'SOCKB连接断开(仅TCP Client)'
+message = {'+EVENT=CON_ON': 'STA 成功连接到路由器 ',
+           '+EVENT=CON_OFF': 'STA 断开路由器或者未连接到路由器 ',
+           '+EVENT=DHCP_OK': 'STA DHCP获取到IP',
+           '+EVENT=SOCKA_ON': 'SOCKA连接建立(仅TCP Client/Server,MQTT,HTTP)',
+           '+EVENT=SOCKA_OFF': 'SOCKA连接断开(仅TCP Client/Server,MQTT,HTTP)',
+           '+EVENT=SOCKB_ON': 'SOCKB连接建立(仅TCP Client)',
+           '+EVENT=SOCKB_OFF': 'SOCKB连接断开(仅TCP Client)',
+           'AT+CMDPW': '设置/查询透传模式下发送 AT 命令的前导字符',
+           'AT+EVENT': '设置/查询透传模式下事件通知功能',
+           'AT+NETPIDEN': '设置/查询是否显示数据来自哪个通讯通道',
+           'AT+NETPID': '设置/查询通讯通道号标记值',
+           'AT+NETP': '设置/查询 SOCKA 网络协议参数',
+           'AT+SOCKB': '设置/查询 SOCKB 网络协议参数',
+           'AT+WMODE': '设置/查询 Wi-Fi 操作模式',
+           'AT+WSMAC': '设置/查询模块的 MAC 地址参数',
+           'AT+WSSSID': '设置/查询关联 AP 的SSID',
+           'AT+WSKEY': '设置/查询 STA 的加密参数',
+           'AT+WAP': '设置/查询 AP 的 Wi-Fi 配置参数',
+           'AT+WANN': '设置/查询 STA 的网络参数',
+           'AT+TCPDISB=on': '建立 SOCKB 链接',
+           'AT+TCPDISB=off': '断开 SOCKB 链接',
+           'AT+TCPDISB': '建立/断开 SOCKB 链接'}
 
 
 def message_CN(rec_str):
-    if CON_ON in rec_str:
-        rec_str = CON_ON_MESSAGE
-    elif CON_OFF in rec_str:
-        rec_str = CON_OFF_MESSAGE
-    elif DHCP_OK in rec_str:
-        rec_str = DHCP_OK_MESSAGE
-    elif SOCKA_ON in rec_str:
-        rec_str = SOCKA_ON_MESSAGE
-    elif SOCKA_OFF in rec_str:
-        rec_str = SOCKA_OFF_MESSAGE
-    elif SOCKB_ON in rec_str:
-        rec_str = SOCKB_ON_MESSAGE
-    elif SOCKB_OFF in rec_str:
-        rec_str = SOCKB_OFF_MESSAGE
+    for i in message.keys():
+        if i in rec_str:
+            rec_str = message.get(i)
+            break
+    if rec_str == '#&B':
+        rec_str = '心跳包'
     return rec_str
--- pyserial/sent_serial.py
+++ pyserial/sent_serial.py
@@ -6,17 +6,18 @@
 """
 # !/usr/bin/python
 # -*-coding: utf-8 -*-
+from time import sleep
 
 import serial
 import threading
 import binascii
 from datetime import datetime
-import struct
 from message import message_CN
 
 
 class SerialPort:
-    def __init__(self, port, buand):
+    def __init__(self, port, buand, type):
+        self.type = type
         self.port = serial.Serial(port, buand)
         self.port.close()
         if not self.port.isOpen():
@@ -42,11 +43,11 @@ class SerialPort:
                 rec_str = binascii.b2a_hex(rec_str)
                 rec_str = decode(rec_str)
                 rec_str = message_CN(rec_str)
-                # rec_str = decode(rec_str)
-                # data_bytes = data_bytes + rec_str
-                # print('当前数据接收总字节数：'+str(len(data_bytes))+' 本次接收字节数：'+str(len(rec_str)))
-                # print(str(datetime.now()), ':', binascii.b2a_hex(rec_str))
-                print(str(datetime.now()), ':', rec_str)
+                if self.type == '发送串口 指令':
+                    print(str(datetime.now()), ':', "\033[31;1m" + self.type + "\033[0m", rec_str)
+                elif self.type == '接收串口 指令':
+                    print(str(datetime.now()), ':', "\033[32;1m" + self.type + "\033[0m", rec_str)
+                sleep(0.02)
 
 
 def hex_list():
@@ -81,56 +82,15 @@ data_bytes = bytearray()
 
 if __name__ == '__main__':
     # 打开串口
-    mSerial_IOT_sent = SerialPort(sentPort, baudRate)
-    # mSerial_IOT_recv = SerialPort(recvPort, baudRate)
-
-    # # 文件写入操作
-    # filename = input('请输入文件名：比如test.csv:')
-    # dt = datetime.now()
-    # nowtime_str = dt.strftime('%y-%m-%d %I-%M-%S')  # 时间
-    # filename = nowtime_str + '_' + filename
-    # out = open(filename, 'a+')
-    # csv_writer = csv.writer(out)
+    mSerial_IOT_sent = SerialPort(sentPort, baudRate, '发送串口 指令')
+    mSerial_IOT_recv = SerialPort(recvPort, baudRate, '接收串口 指令')
 
     # 开始数据读取线程
-    t1 = threading.Thread(target=mSerial_IOT_sent.read_data)
-    # t2 = threading.Thread(target=mSerial_IOT_recv.read_data)
+    t1 = threading.Thread(target=mSerial_IOT_sent.read_data, name='IOT SENT')
+    t2 = threading.Thread(target=mSerial_IOT_recv.read_data, name='IOT RECV')
 
-    t1.setDaemon(True)
+    # t1.setDaemon(True)
     # t2.setDaemon(True)
 
     t1.start()
-    # t2.start()
-
-    # while not is_exit:
-    #     # 主线程:对读取的串口数据进行处理
-    #     data_len = len(data_bytes)
-    #     i = 0
-    #     while (i < data_len - 1):
-    #         if (data_bytes[i] == 0xFF and data_bytes[i + 1] == 0x5A):
-    #             frame_code = data_bytes[i + 2]
-    #             frame_len = struct.unpack('<H', data_bytes[i + 4:i + 6])[0]
-    #             frame_time = struct.unpack('<I', data_bytes[i + 6:i + 10])[0]
-    #             print('帧类型：', frame_code, '帧长度：', frame_len, '时间戳：', frame_time)
-    #             print(frame_code, frame_len, frame_time)
-    #             if frame_code == 0x03:  # 判断帧类型
-    #                 # struct 解析数据帧
-    #                 accelerated_x, accelerated_y, accelerated_z, angular_x, angular_y, angular_z, tem, speed_x, speed_y, speed_z, \
-    #                 angular_v_x, angular_v_y, angular_v_z = struct.unpack('<fffffffffffff',
-    #                                                                       data_bytes[i + 12:i + 12 + frame_len - 6])
-    #                 dt = datetime.now()
-    #                 nowtime_str = dt.strftime('%y-%m-%d %I:%M:%S')  # 时间
-    #                 loc_str = [nowtime_str, frame_time, accelerated_x, accelerated_y, accelerated_z, angular_x,
-    #                            angular_y, angular_z, tem, speed_x, speed_y, speed_z, \
-    #                            angular_v_x, angular_v_y, angular_v_z]
-    #                 print(loc_str)
-    #
-    #                 # # 写入csv文件
-    #                 # try:
-    #                 #     csv_writer.writerow(loc_str)
-    #                 # except Exception as e:
-    #                 #     raise e
-    #             i = i + 6 + frame_len + 3
-    #         else:
-    #             i = i + 1
-    #     data_bytes[0:i] = b''
+    t2.start()
