f7c9618b35fdc3d8c4e6641df483d4158dbc4f49
310486578@qq.com <ff422433233>
2022-01-09 23:25:27 +0800

    Django 学习

--- student_sys/student_sys/urls.py
+++ student_sys/student_sys/urls.py
@@ -15,11 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
 
 urlpatterns = [
     path('', include('student.urls')),
     path('admin/', admin.site.urls),
-    # url(r'^$', index, name='index'),
-    # url(r'^admin', admin.site.urls)
 ]
